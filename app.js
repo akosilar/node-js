00cafcd27ad273f732ee45a2945cf0c6ea765b20
akosilar <55524614+akosilar@users.noreply.github.com>
2022-09-10 16:45:40 -0400

    after app.delete

--- app.js
+++ app.js
@@ -136,7 +136,17 @@ app.get('/blogs/:id', (req,res) => {
         })
 });
 
+app.delete('/blogs/:id', (req,res) => {
+    const id = req.params.id;
 
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({redirect: '/blogs'})
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+})
 
 //redirects
 app.get('/about-us', (req,res) => {
